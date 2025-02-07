9916f21804cce70f3a561d92d4c5f2f35e0f006d
Tiff Huang <tiffany@headstart.nyc>
2025-02-07 15:52:01 -0500

    update url

--- friday_auto_coder.rb
+++ friday_auto_coder.rb
@@ -1,7 +1,7 @@
 class FridayAutoCoder < Formula
   desc "Friday AutoCoder"
   homepage "https://github.com/HeadstartAI/auto_coder"
-  url "https://github.com/HeadstartAI/auto_coder/archive/refs/tags/1.1.tar.gz"
+  url "https://raw.githubusercontent.com/HeadstartAI/auto_coder/1.1/archive.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   # Add your dependencies
