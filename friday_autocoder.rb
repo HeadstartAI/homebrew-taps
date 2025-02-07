f3c7efb072dcb4e09ea31ae635ad168bab8cb805
Tiff Huang <tiffany@headstart.nyc>
2025-02-07 15:45:22 -0500

    Add friday brew formula

---
+++ friday_autocoder.rb
@@ -0,0 +1,28 @@
+class FridayAutoCoder
+  desc "Friday AutoCoder"
+  homepage "https://github.com/HeadstartAI/auto_coder"
+  url "https://github.com/HeadstartAI/auto_coder/archive/refs/tags/1.1.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  # Add your dependencies
+  depends_on "poetry"
+  depends_on "gh"
+  depends_on "fd"
+  depends_on "ripgrep"  # this is the package name for 'rg'
+
+  def install
+    # Run poetry install
+    system "poetry", "install"
+
+    # Install your script
+    bin.install "bin/run" => "run"
+
+    # Make the script executable
+    system "chmod", "+x", "#{bin}/run"
+  end
+
+  test do
+    system "#{bin}/run"
+  end
+end
+
