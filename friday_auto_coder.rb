fb076fb2a716313b7cca93970d3585634d5b95fd
Tiff Huang <tiffany@headstart.nyc>
2025-02-07 16:13:00 -0500

    update url and sha

--- friday_auto_coder.rb
+++ friday_auto_coder.rb
@@ -2,7 +2,7 @@ class FridayAutoCoder < Formula
   desc "Friday AutoCoder"
   homepage "https://github.com/HeadstartAI/auto_coder"
   url "https://api.github.com/repos/HeadstartAI/auto_coder/tarball/1.1"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "f3450ceffb845eb13bd525dac83655bc5b50b18d5519e7c83aadf9702c947132"
 
   # Add your dependencies
   depends_on "poetry"
