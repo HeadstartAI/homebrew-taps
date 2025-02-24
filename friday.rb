c4e4697445f06e0d3a8a0b439a3b106145d3e058
Tiff Huang <tiffany@headstart.nyc>
2025-02-24 15:24:27 -0500

    bump revision & change env var name

--- friday.rb
+++ friday.rb
@@ -2,12 +2,13 @@ class Friday < Formula
   desc "Friday"
   homepage "https://github.com/HeadstartAI/auto_coder"
   version "stable"
+  revision 1
 
   on_arm do
     url "https://api.github.com/repos/HeadstartAI/auto_coder_releases/releases/assets/232183233", using: :curl,
       follow_location: true,
       headers: ["Accept: application/octet-stream",
-               "Authorization: token #{ENV['HOMEBREW_GITHUB_API_TOKEN']}"]
+               "Authorization: token #{ENV['FRIDAY_GITHUB_API_TOKEN']}"]
     sha256 "5c9b3f2e9c0ba52afdea825b739e2472b3cb8e65d376796425aa8f1465d7257e"
   end
 
