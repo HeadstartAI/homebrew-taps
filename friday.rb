56e06cd39bb945f8415178689f02c583af660c47
Tiff Huang <tiffany@headstart.nyc>
2025-03-13 19:17:42 -0400

    release 1.1.8

--- friday.rb
+++ friday.rb
@@ -2,14 +2,14 @@ class Friday < Formula
   desc "Friday"
   homepage "https://github.com/HeadstartAI/friday_releases"
   version "stable"
-  revision 16
+  revision 17
 
   # Use a conditional URL based on architecture
   if Hardware::CPU.arm?
-    url "https://api.github.com/repos/HeadstartAI/friday_releases/releases/assets/235548160", using: :curl,
+    url "https://api.github.com/repos/HeadstartAI/friday_releases/releases/assets/237285078", using: :curl,
       follow_location: true,
       headers: ["Accept: application/octet-stream"]
-    sha256 "d46dcec21bee9dfbea271862897714fccf3b665f1f2c34dad37e8efea8521920"
+    sha256 "cc1e66baa8d9b14ebeae5f1f8be921b18aaaa30379b4764b4bb79d60c8c03de7"
   else
     # Dummy URL to satisfy Homebrew
     url "https://api.github.com/repos/HeadstartAI/friday_releases/releases/latest", using: :curl,
