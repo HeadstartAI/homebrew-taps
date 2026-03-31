858a3aca24dfdf8651452276fc13fb77ea4950be
Tiffany Huang <tiffany@headstart.nyc>
2026-03-31 10:10:49 -0400

    Update Friday Beta to 3.7.0 (#195)

--- friday-beta.rb
+++ friday-beta.rb
@@ -1,14 +1,14 @@
 class FridayBeta < Formula
   desc "Friday (Beta channel)"
   homepage "https://github.com/HeadstartAI/friday_releases"
-  version "3.6.0"
+  version "3.7.0"
 
   # Use a conditional URL based on architecture
   if Hardware::CPU.arm?
-    url "https://api.github.com/repos/HeadstartAI/friday_releases/releases/assets/382849706", using: :curl,
+    url "https://api.github.com/repos/HeadstartAI/friday_releases/releases/assets/385234422", using: :curl,
       follow_location: true,
       headers: ["Accept: application/octet-stream"]
-    sha256 "48f2a4bab080417370a5b6aaddde973ac8f8a52c9662fd379172dbc42a2197db"
+    sha256 "90f8920e26697e84aa1e8a9fb8fef60533be83a964a2c2b9e3e714f43cb8ddc5"
   else
     # Dummy URL to satisfy Homebrew
     url "https://api.github.com/repos/HeadstartAI/friday_releases/releases/tags/beta", using: :curl,
